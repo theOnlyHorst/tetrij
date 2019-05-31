a12c786cc990470becc122696d2885023c188855
theOnlyHorst <buschsteffmichi@chello.at>
2019-06-01 00:41:18 +0200

    mini optimization

--- src/main/java/at/theOnlyHorst/tetrij/gameTasks/RenderScreen.java
+++ src/main/java/at/theOnlyHorst/tetrij/gameTasks/RenderScreen.java
@@ -1,42 +1,44 @@
 package at.theOnlyHorst.tetrij.gameTasks;
 
-import at.theOnlyHorst.tetrij.TetriJ;
-import org.lwjgl.opengl.ARBVertexBufferObject;
-
 import static org.lwjgl.opengl.GL30.*;
 
 public class RenderScreen extends AbstractRenderTask {
 
 
     float[] vertices = {
-          0,1,0,
-          0,0,0,
-          1,0,0,
+          0,1,
+          0,0,
+          1,0,
+          0,1,
+          1,1,
+          1,0
+    };
+    float[] textureCoords ={
+
     };
 
-    float[] colors = {
-        1,0,0,1
-    };
+
+
+    int bao;
+
 
     @Override
     public void start() {
+        bao = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER,bao);
+        glBufferData(GL_ARRAY_BUFFER,vertices,GL_STATIC_DRAW);
 
     }
 
     @Override
     public void render(double lagDelta, long deltaTime) {
 
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-            int bao = glGenBuffers();
-            glBindBuffer(GL_ARRAY_BUFFER,bao);
-            glBufferData(GL_ARRAY_BUFFER,vertices,GL_STATIC_DRAW);
-            glEnableVertexAttribArray(0);
-            glBindBuffer(GL_ARRAY_BUFFER,bao);
-            glVertexAttribPointer(0,3,GL_FLOAT,false,0,0);
-            glDrawArrays(GL_TRIANGLES,0,3);
-            glDisableVertexAttribArray(0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER,bao);
+        glVertexAttribPointer(0,2,GL_FLOAT,false,0,0);
+        glDrawArrays(GL_TRIANGLES,0,3*2);
+        glDisableVertexAttribArray(0);
 
     }
 }
